e64de82299ef8bad76879bd74edf31a9f2b24687
Mukherjee, Srabani <srabmukherjee@deloitte.com>
2025-05-16 11:45:18 +0530

    committing updates practice file

--- src/test/java/uiAutomation/WindowHandles.java
+++ src/test/java/uiAutomation/WindowHandles.java
@@ -1,32 +1,33 @@
 package uiAutomation;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
+import java.util.Set;
 
 public class WindowHandles {
 
     public static void main(String[] args) throws InterruptedException {
-
-
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.get("https://www.hyrtutorials.com/p/window-handles-practice.html");
-        /*String current_window = driver.getWindowHandle();
-        driver.findElement(By.xpath("//div/p/button[text()='New Browser Tab']")).click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        driver.manage().window().maximize();
+        String parentWindow = driver.getWindowHandle();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//button[@id='newWindowBtn']")).click();
         Thread.sleep(5000);
-        String source_link = "//p/span/a[contains(text(),'Decision Models')]";
-
-        Set<String> all_windows = driver.getWindowHandles();
-        Iterator<String> itr = all_windows.iterator();
-
-        while(itr.hasNext()){
-            if(current_window.equalsIgnoreCase(itr.next()))
-            itr.next();;
-
-        }*/
-
-
+        Set<String> handles = driver.getWindowHandles();
+        for(String handle : handles){
+            if(!handle.equals(parentWindow)){
+                driver.switchTo().window(handle);
+                driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("hii");
+                driver.close();
+                Thread.sleep(5000);
+            }
+        }
+        driver.switchTo().window(parentWindow);
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("Hello");
     }
 }
